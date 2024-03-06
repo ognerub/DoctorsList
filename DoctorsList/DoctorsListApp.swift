af5722c4475b5c1243a4e0e2d5340160e8a2b8b0
ognerub <a.ulianovskiy@yandex.ru>
2024-03-06 18:41:28 +0300

    Initial Commit

---
+++ DoctorsList/DoctorsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorsListApp.swift
+//  DoctorsList
+//
+//  Created by Admin on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DoctorsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
